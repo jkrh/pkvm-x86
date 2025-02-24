2186a647923d923b7af09040f4fee9782850feb2
Markku Ahvenjärvi <mankku@gmail.com>
2025-02-24 10:52:29 +0200

    sysroot-wrapper: support component variables
    Add support for defining per component variables that are passed to
    isolated sysroot. The variables are listed in a space separated list
    variable <COMPONENT>_VARIABLES_WHITELIST, and are injected if the
    variable exists.
    
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- scripts/sysroot-wrapper.sh
+++ scripts/sysroot-wrapper.sh
@@ -10,7 +10,7 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 usage() {
 	cat <<EOF
-Usage: $0 [OPTS] SCRIPT [ARGS]
+Usage: ${0} [OPTS] SCRIPT [ARGS]
 
 Execute build script in an isolated sysroot.
 
@@ -34,6 +34,24 @@ SCRIPT          Build script for the component.
 
 Options:
   -h|--help     Show this.
+
+Variables:
+  <COMPONENT>_VARIABLES_WHITELIST variable can be used to "pass-through"
+  variables to isolated sysroot environment.
+
+  Example:
+    $ export BAR=BAZ
+    $ export FOO_VARIABLES_WHITELIST="BAR QUX"
+    $ # Injects BAR=BAZ and QUX=QUUX
+    $ QUX=QUUX ${0} ./build-foo.sh
+
+  SYSROOT_EXTRA_VARS variable can be used to set variables to isolated sysroot
+  environment.
+
+  Example:
+    $ export SYSROOT_EXTRA_VARS="FOO='BAR BAZ' QUX=QUUX"
+    $ # Injects FOO='BAR BAZ' and QUX=QUUX
+    $ ${0} ./build-foo.sh
 EOF
 }
 
@@ -66,10 +84,21 @@ export COMPONENT_BUILD_DIR=$BASE_DIR/build/${COMPONENT}
 # Directories inside chroot
 export COMPONENT_CHROOT_DIR="${COMPONENT_BUILD_DIR#"${BASE_DIR%/}"}"
 
-copy_sources()
-{
+copy_sources() {
 	rsync -aWt --filter=":- .gitignore" --no-compress \
 		"$COMPONENT_DIR" "$(dirname "$COMPONENT_BUILD_DIR")"
+}
+
+set_sysroot_variables() {
+	# Set whitelisted variables for the component
+	whitelist=${COMPONENT^^}_VARIABLES_WHITELIST
+	read -ra whitelist <<< "${!whitelist}"
+
+	[[ -v SYSROOT_EXTRA_VARS_ARR ]] || declare -ag SYSROOT_EXTRA_VARS_ARR
+	for var in "${whitelist[@]}"; do
+		[ -v "$var" ] && SYSROOT_EXTRA_VARS_ARR+=("$var=${!var}")
+	done
+	return 0
 }
 
 copy_sources
@@ -77,9 +106,11 @@ copy_sources
 sysroot_set_trap build_sysroot_unmount_all
 build_sysroot_mount_all
 
+# Set whitelisted variables for the component
+set_sysroot_variables
+
 sysroot_run_commands "$BUILD_SYSROOT_DIR" \
-	"set -x
-	cd /build/
+	"cd /build/
 	export BASE_DIR=\$(pwd)
 	exec $(printf '%q ' "$@")
 	"

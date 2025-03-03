667fe376065346ef23ae05629a26907de36e3ce9
Markku Ahvenjärvi <markku.ahvenjarvi@tii.ae>
2025-03-03 10:51:05 +0200

    guest-kernel: use common variables
    Signed-off-by: Markku Ahvenjärvi <markku.ahvenjarvi@tii.ae>

--- scripts/build-guest-kernel.sh
+++ scripts/build-guest-kernel.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env -S bash -e
 
-NJOBS=`nproc`
-BASEDIR=$PWD
+[ ! -d "$BASE_DIR" ] && sysroot_exit_error 1 "BASE_DIR does not exist"
 
 copy_kernel()
 {
-	[ ! -d "$BASEDIR/build/linux" ] && mkdir -p "$BASEDIR/build/linux"
-	rsync -aWt --filter=":- .gitignore" --no-compress "$BASEDIR/linux" "$BASEDIR/build/"
+	[ ! -d "$BASE_DIR/build/linux" ] && mkdir -p "$BASE_DIR/build/linux"
+	rsync -aWt --filter=":- .gitignore" --no-compress "$BASE_DIR/linux" "$BASE_DIR/build/"
 }
 
 copy_kernel
 
-cd $BASEDIR/build/linux
+cd "$BASE_DIR/build/linux"
 make CC="$CC" -j$(nproc) nixos_guest_defconfig bzImage modules

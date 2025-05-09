aa82054759091664eb6012deb45ca3bbec35a58b
Markku Ahvenjärvi <mankku@gmail.com>
2025-05-09 10:25:35 +0300

    run: enable pkvm in host kernel command line args
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- platform/q35/vars.mk
+++ platform/q35/vars.mk
@@ -1,1 +1,1 @@
-KERNEL_OPTS := root=/dev/vda1 console=ttyS0 mem=8G nokaslr ignore_loglevel intel_iommu=sm_on rw csdlock_debug=1 earlyprintk=ttyS0
+KERNEL_OPTS := root=/dev/vda1 console=ttyS0 mem=8G nokaslr ignore_loglevel intel_iommu=sm_on rw csdlock_debug=1 earlyprintk=ttyS0 kvm-intel.pkvm=1

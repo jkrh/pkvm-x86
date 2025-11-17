d9e68b5a5aa3365b9db2518d13e5ec4a7479367c
Jani Hyvonen <jani.k.hyvonen@gmail.com>
2025-11-17 17:16:54 +0200

    q35: usb image: add signed efi boot app to usb
    Additionally this patch adds support for copying
    additional signed (test) application into USB.
    
    Also VNC support for viewing QEMU window remotely
    is added.
    
    Signed-off-by: Jani Hyvonen <jani.k.hyvonen@gmail.com>

--- scripts/mkusbstickimg.sh
+++ scripts/mkusbstickimg.sh
@@ -4,8 +4,12 @@ set -e
 IMG_FILE=$1
 IMG_SIZE=$2
 UEFI_BOOT_APP=$3
+KSMI_TEST_APP=$4
 VOLUME_LABEL="12345"
 MARKER_FILE="KS.BAK"
+
+[ -z "$PRIVATEKEY" ] && PRIVATEKEY="$BASE_BUILD_DIR/keydata/MOK-DB.priv"
+[ -z "$PUBLICKEY" ] && PUBLICKEY="$BASE_BUILD_DIR/keydata/MOK-DB.pem"
 
 echo "Creating clean ${IMG_SIZE_MB}MB raw disk image..."
 dd if=/dev/zero of="${IMG_FILE}" bs=1M count=${IMG_SIZE}
@@ -31,14 +35,33 @@ sudo mount -o loop,offset=$((2048 * 512)) "${IMG_FILE}" "${MOUNT_POINT}"
 # Create the empty marker file
 sudo touch "${MOUNT_POINT}/${MARKER_FILE}"
 # Add uefi boot app if requested
-if [ -f "${UEFI_BOOT_APP}" ]; then
+if [ -n "${UEFI_BOOT_APP}" ] && [ -f "${UEFI_BOOT_APP}" ]; then
   echo "Adding ${UEFI_BOOT_APP} to USB media"
   sudo mkdir -p ${MOUNT_POINT}/efi/boot
-  sudo cp ${UEFI_BOOT_APP} ${MOUNT_POINT}/efi/boot/BOOTX64.efi
+	sbsign  --key $PRIVATEKEY \
+		--cert $PUBLICKEY ${UEFI_BOOT_APP} \
+		--output $BASE_BUILD_DIR/BOOTX64.app.efi.signed
+  sudo cp $BASE_BUILD_DIR/BOOTX64.app.efi.signed ${MOUNT_POINT}/efi/boot/BOOTX64.efi
+fi
+# SMI test application
+if [ -n "${KSMI_TEST_APP}" ] && [ -f "${KSMI_TEST_APP}" ]; then
+  echo "Adding ${KSMI_TEST_APP} to USB media"
+  sudo mkdir -p ${MOUNT_POINT}/efi/boot
+	sbsign  --key $PRIVATEKEY \
+		--cert $PUBLICKEY ${KSMI_TEST_APP} \
+		--output $BASE_BUILD_DIR/KSMItApp.efi.signed
+  sudo cp $BASE_BUILD_DIR/KSMItApp.efi.signed ${MOUNT_POINT}/efi/boot/KSMItApp.efi
 fi
 
 # Unmount and clean up
-sudo umount "${MOUNT_POINT}"
+sudo sync
+sudo umount -l "${MOUNT_POINT}"
+
+while grep -qs "$MOUNT_POINT" /proc/mounts; do
+  printf "."
+  sleep 1
+done
+
 rmdir "${MOUNT_POINT}"
 
 echo "Done, compatible image '${IMG_FILE}' is ready."
